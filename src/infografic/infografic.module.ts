4c2682b7977473f96e85714dc5f2efe7ee23f4f2
iyoubee <al.ayubi2020@gmail.com>
2023-07-08 12:13:33 +0700

    feat: done be

--- src/infografic/infografic.module.ts
+++ src/infografic/infografic.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { InfograficService } from './infografic.service';
 import { InfograficResolver } from './infografic.resolver';
 import { DbService } from 'src/providers/database/db';
+import { InfograficRepositories } from 'src/models/infografic.repo';
 
 @Module({
-  providers: [InfograficResolver, InfograficService, DbService],
-  exports: [DbService],
+  providers: [
+    InfograficResolver,
+    InfograficService,
+    DbService,
+    InfograficRepositories,
+  ],
+  exports: [DbService, InfograficRepositories],
 })
 export class InfograficModule {}
